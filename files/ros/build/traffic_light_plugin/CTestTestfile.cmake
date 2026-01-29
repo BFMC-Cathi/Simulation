73d2f4bfcc17f1c2b2b3ed161fa2bd813a0674b2
kutmur <halilkutmur@gmail.com>
2026-01-29 16:38:55 +0300

    Deletion of Unnecessary file

--- files/ros/build/traffic_light_plugin/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ros_dev/BFMC_workspace/files/ros/src/traffic_light_plugin
-# Build directory: /home/ros_dev/BFMC_workspace/files/ros/build/traffic_light_plugin
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
